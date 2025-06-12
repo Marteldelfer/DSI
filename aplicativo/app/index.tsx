1f7042567ea73d007dcdcea84a50505b4ba9ca0f
malucor <luiza1772@gmail.com>
2025-06-12 00:23:30 -0300

    tela de login abre primeiro

--- aplicativo/app/index.tsx
+++ aplicativo/app/index.tsx
@@ -2,11 +2,11 @@ import { Text, View } from "react-native";
 
 import { useRouter } from "expo-router";
 
-import TelaCadastro from "./telas/Cadastro";
+import TelaLogin from "./telas/Login"; //
 
 
 export default function Index() {
   return (
-    <TelaCadastro></TelaCadastro>
+    <TelaLogin></TelaLogin>
   );
 }
